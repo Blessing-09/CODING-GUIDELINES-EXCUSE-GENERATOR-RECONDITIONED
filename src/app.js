3a9e523ccbac0d5bd757dca3d09b86fe80bf22e8
Blessing-09 <blessingogbogu@gmail.com>
2025-03-10 01:51:45 +0100

    First commit

--- src/app.js
+++ src/app.js
@@ -2,43 +2,53 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// I called all the HTML elements that I want to work with by their id and stored them in a variable
-let excuse = document.getElementById("excuse");
-let name = document.getElementById("name");
-let image = document.getElementById("image");
-let button1 = document.getElementById("button1");
-let button2 = document.getElementById("button2");
-let text = document.getElementById("text");
-// I declared my arrays in a variable with the values I would like to work with
-let greeting = ['Hello!', 'Hey!', 'Hi!', 'Hey you!', 'Hi everyone!'];
-let introduction = ['My name is', 'They call me', 'I go by the name', 'Just call me'];
-let namePers =['Wonderwoman', 'Hulk', 'Spiderman', 'Gamora', 'Ironman', 'Catwoman'];
-let action = ['I love to', 'I like to', 'I hate to'];
-let what = ['cook 🍳', 'dance 🪩', 'travel ✈️', 'sleep 😴', 'fight villains🦹‍♂️', 'save the world🌍', 'code 👩‍💻', 'eat 🍽', 'play 🛝', 'read 📖', 'watch movies 🍿', 'listen to music 🎵'];
-let bgColor = ['pink', 'grey', 'tomato'];
-let textColor =["white", "blue", "green", "purple"];
- let imageSrc = ["https://i.ibb.co/YTdNjv4r/5eb241180c6dba55cd27d57d6bc34239.jpg", "https://i.ibb.co/WWhYMtRf/878840125767.jpg", "https://i.ibb.co/0pM0RL54/Gamora.webp", "https://i.ibb.co/XxjCGJwt/Iron-Man-AOU-Render.webp", "https://i.ibb.co/Q7pFLY06/spiderman-art-design-icon-vector-600nw-2404385831.webp", "https://i.ibb.co/s9kKwvSM/Wonder-Woman-DC-Comics-Justice-League-Lifesize-Cardboard-Cutout-available-now-at-starstills-73279.jpg"];
+
+
+//! DOM elements are marked with $
+const $ = selector => document.querySelector(selector);
+const $excuse = $("#excuse"); 
+const $nameText = $("#name"); 
+const $button1 = $("#button1");
+const $button2 = $("#button2");
+const $text = $("#text");
+const $image = $("#image");
 
 // Window function 
-window.onload = function() {
-  //When the window loads, the button2 is hidden, to be used later. We can only make use of the button1
+window.onload = () => {
+    $image.src = "https://i.ibb.co/20QcB9pW/Its-Game-Time.jpg";
+    $button2.hidden = true;
   
-  image.src = "https://i.ibb.co/20QcB9pW/Its-Game-Time.jpg";
-  button2.style.display = "none";
-  
-  button1.addEventListener('click', function () {
-    //When the button1 is clicked, the button2 is shown, and the initial text is hidden
-    image.src = "https://i.ibb.co/MDL5XBzn/start.png";
-    text.style.display = "none";
-    excuse.textContent = "On your mark...get set... ⬇";
-    button2.style.display = "inline-block";
+    $button1.addEventListener('click', () => {
+      $image.src = "https://i.ibb.co/MDL5XBzn/start.png";
+      $text.hidden = true;
+      $excuse.textContent = "On your mark...get set... ⬇";
+      $button2.hidden = false;
+  });
 
-    button2.addEventListener('click', function () {
-      //When the button2 is clicked, the following take place:
-      // However, for easier manipulation, I have stored the random values generated in a function in variables
+    $button2.addEventListener('click', () => {
+      let greeting = ['Hello!', 'Hey!', 'Hi!', 'Hey you!', 'Hi everyone!'];
+      let introduction = ['My name is', 'They call me', 'I go by the name', 'Just call me'];
+      let namePers =['Hulk', 'Catwoman', 'Gamora', 'Ironman','Spiderman','Wonderwoman'];
+      let action = ['I love to', 'I like to', 'I hate to'];
+      let what = ['cook 🍳', 'dance 🪩', 'travel ✈️', 'sleep 😴', 'fight villains🦹‍♂️', 'save the world🌍', 'code 👩‍💻', 'eat 🍽', 'play 🛝', 'read 📖', 'watch movies 🍿', 'listen to music 🎵'];
+      let bgColor = ['pink', 'grey', 'tomato'];
+      let textColor =["white", "blue", "green", "purple"];
+      let imageSrc = [
+        "https://i.ibb.co/YTdNjv4r/5eb241180c6dba55cd27d57d6bc34239.jpg", 
+        "https://i.ibb.co/WWhYMtRf/878840125767.jpg", 
+        "https://i.ibb.co/0pM0RL54/Gamora.webp", 
+        "https://i.ibb.co/XxjCGJwt/Iron-Man-AOU-Render.webp", 
+        "https://i.ibb.co/Q7pFLY06/spiderman-art-design-icon-vector-600nw-2404385831.webp", 
+        "https://i.ibb.co/s9kKwvSM/Wonder-Woman-DC-Comics-Justice-League-Lifesize-Cardboard-Cutout-available-now-at-starstills-73279.jpg"
+        ];
+
+function getRandomItem(arr){
+  return Math.floor(Math.random() * arr.length);
+}
+
       let randomGreeting = getRandomItem(greeting);
       let randomIntroduction = getRandomItem(introduction);
       let randomName = getRandomItem(namePers);
@@ -47,36 +57,31 @@ window.onload = function() {
       let randomImage = getRandomItem(imageSrc);
       let randomColor = getRandomItem(bgColor);
       let randomTextColor = getRandomItem(textColor);
-      // When the button2 is clicked, random values are generated and displayed
-      name.innerText = randomGreeting;
-      excuse.style.backgroundColor = randomColor;
-      excuse.style.color = randomTextColor;
-      excuse.innerHTML= `${randomIntroduction} <h5 style="color:black; display:inline;">${randomName}⚡️.</h5> ${randomAction} ${randomWhat}`;
-      image.src = randomImage;
       
-      // The if condition creates a match between each character and their name. If that happens, an alert is shown and the match is displayed after clicking the "ok"
-      if (
-        randomName === namePers[0] && randomImage === imageSrc[5]|| 
-        randomName === namePers[1] && randomImage === imageSrc[0]|| 
-        randomName === namePers[2] && randomImage === imageSrc[4]|| 
-        randomName === namePers[3] && randomImage === imageSrc[2]||
-        randomName === namePers[4] && randomImage === imageSrc[3]|| 
-        randomName === namePers[5] && randomImage === imageSrc[1]
-      )
+      $nameText.innerText = greeting[randomGreeting];
+      $excuse.style.backgroundColor = bgColor[randomColor];
+      $excuse.style.color = textColor[randomTextColor];
+      $excuse.innerHTML= `
+        ${introduction[randomIntroduction]} 
+        <h5 style="color:black; display:inline;">${namePers[randomName]}⚡️.</h5> 
+        ${action[randomAction]} 
+        ${what[randomWhat]}
+      `;
+      $image.src = imageSrc[randomImage];
+      
+      if (randomName === randomImage) {
+        setTimeout(() => {
+          $button2.textContent = "Let's play again!";
+          alert('YAY!!!🥳 We found a MATCH!!!! 🎉🥳🎊'); 
+        }, 10); // Delay for 2 seconds
+      }
+      else {
+          $button2.textContent = "SHUFFLE!";
+      }
 
-        {
-          button2.textContent = "Let's play again!";
-          alert('YAY!!!🥳 We found a MATCH!!!! 🎉🥳🎊');
-         }
-         else {
-          button2.textContent = "SHUFFLE!";
-         }
-   
     });
-  });
-};
 
-function getRandomItem(arr){
-  let randomIndex=Math.floor(Math.random() * arr.length);
-  return arr[randomIndex];
-}
+  };
+
+
+
